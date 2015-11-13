2242a708de1e3a7e2faefd6ea9c7f04c08f6d45f
arielcessario <arielcessario@gmail.com>
2015-11-13 09:11:06 -0300

    v01

--- app/app.js
+++ app/app.js
@@ -185,7 +185,7 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
         var conErrores = false;
 
         if (vm.usuario.cliente_id == -1) {
-            AcUtilsService.validations('nombre', 'Esta opción es solo para modificación, debe seleccionar un usuario');
+            AcUtilsService.validations('nombre', 'Esta opciï¿½n es solo para modificaciï¿½n, debe seleccionar un usuario');
             conErrores = true;
             return;
         }
@@ -403,19 +403,25 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
     };
 
 
-    /* @description: Función que verifica si es celu o no.
+    /* @description: Funciï¿½n que verifica si es celu o no.
      */
     window.mobilecheck = function () {
         var check = false;
+        //(function (a) {
+        //    //console.log(a);
+        //    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))check = true
+        //})(navigator.userAgent || navigator.vendor || window.opera);
+
         (function (a) {
-            //console.log(a);
-            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))check = true
+            if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))check = true
         })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
     };
 
 
-    // Si es if(vm.isMobile), significa que después de ahí vienen cosas para celu.
+
+
+    // Si es if(vm.isMobile), significa que despuï¿½s de ahï¿½ vienen cosas para celu.
     vm.isMobile = window.mobilecheck();
     //console.log(vm.isMobile);
 
@@ -423,7 +429,7 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
     // Contenedor Principal
     var mainContainer = angular.element(document.querySelector('#main-container'));
 
-    // Ancho de la pantalla del cliente - No es la resolución es al ancho interno del explorador
+    // Ancho de la pantalla del cliente - No es la resoluciï¿½n es al ancho interno del explorador
     var mainWidth = mainContainer[0].clientWidth;
 
     // Alto de la pantalla del cliente - Es el alto interno del explorador
@@ -435,16 +441,16 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
     // Grupo que contiene todas las capas pero no es el contenedor principal, sirve para parallax
     var group1 = angular.element(document.querySelector('#group1'));
 
-    // Sección del fondo, la que tiene la imagen de los poligonos
+    // Secciï¿½n del fondo, la que tiene la imagen de los poligonos
     var backgroundLayer = angular.element(document.querySelector('#background-layer'));
 
-    // Sección del que está adelante de los productos - fore
+    // Secciï¿½n del que estï¿½ adelante de los productos - fore
     var fore = angular.element(document.querySelector('#layer-fore'));
 
-    // Sección del que está mas adelante de todo - fore2
+    // Secciï¿½n del que estï¿½ mas adelante de todo - fore2
     var fore2 = angular.element(document.querySelector('#layer-fore2'));
 
-    // Div final, el que tiene el formulario de contacto y todo lo demás.
+    // Div final, el que tiene el formulario de contacto y todo lo demï¿½s.
     var form = angular.element(document.querySelector('#form'));
 
     // Todas las secciones - seccion-01, seccion-02, etc
@@ -469,8 +475,8 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
         }
 
         var multiplicador = (mainWidth * 13) + margen;
-        // Son 12 secciones - 10 de fotos - 1 presentación - 1 contacto
-        // Las hago de 1.5 de ancho cada sección para tener espacio para recorrer cuando estén fijos productos
+        // Son 12 secciones - 10 de fotos - 1 presentaciï¿½n - 1 contacto
+        // Las hago de 1.5 de ancho cada secciï¿½n para tener espacio para recorrer cuando estï¿½n fijos productos
         seccionPrincipal[0].style.maxWidth = multiplicador + 'px';
         seccionPrincipal[0].style.width = multiplicador + 'px';
 
@@ -481,10 +487,10 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
         }
 
 
-        // Tamaño del grupo parallax
+        // Tamaï¿½o del grupo parallax
         group1[0].style.minWidth = multiplicador + 'px';
 
-        // tamaño del layer mas al fondo
+        // tamaï¿½o del layer mas al fondo
         if (vm.isMobile) {
             backgroundLayer[0].style.minWidth = ((mainWidth * 12) + (margen * 14)) + 'px';
         } else {
@@ -517,13 +523,13 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
             }
 
             if (vm.isMobile) {
-                // Acá adentro las cosas para celu
+                // Acï¿½ adentro las cosas para celu
                 secciones[i].style.position = 'relative';
                 if (i != 0) {
                     secciones[i].style.marginTop = (-1 * (mainWidth / 1.2)) + 'px';
                 }
 
-                // Deja fija a la sección de contacto.
+                // Deja fija a la secciï¿½n de contacto.
                 if (i == secciones.length - 1) {
                     secciones[i].style.marginTop = '0px';
                 }
@@ -536,7 +542,7 @@ function MainController($scope, $timeout, $http, store, LoginService, AcUtilsSer
                 if (i < secciones.length - 1) {
                     secciones[i].style.marginTop = (mainWidth / 8) + 'px';
                 } else {
-                    // Todo lo que está acá solo aplica al formulario final
+                    // Todo lo que estï¿½ acï¿½ solo aplica al formulario final
                 }
 
 
